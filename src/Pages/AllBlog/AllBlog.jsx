fb47ac329d7ccc2ec331ff89d106e69cf4b2d95b
Rasibul <rasibul179@gmail.com>
2023-11-06 19:06:05 +0600

    degin input search field and filter by title for serch title and get card

--- src/Pages/AllBlog/AllBlog.jsx
+++ src/Pages/AllBlog/AllBlog.jsx
@@ -1,27 +1,34 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import AllBlogcCart from "../../Components/AllBlogCart/AllBlogcCart";
 import { FaSearch } from "react-icons/fa";
+import { useEffect, useState } from "react";
 
 
 const AllBlog = () => {
     const allBlogs = useLoaderData()
+    const [searchTerm,setSearchTerm] = useState('')
     return (
         <div>
             <h2 className="text-center text-base sm:text-3xl font-bold py-5">All Blogs Here</h2>
             <div className="py-4 max-w-[1300px]  mx-auto">
                 <h2 className=" text-base sm:text-2xl font-bold">Search Your Favourite Blog</h2>
-                <form className="form-control">
-                    <div className="input-group">
-                        <input type="text" placeholder="Search…" className="input input-bordered" />
+                <div className="form-control">
+                    <form className="input-group">
+                        <input type="text" placeholder="Search…" className="input input-bordered" onChange={(e) => setSearchTerm(e.target.value)} />
                         <button className="btn btn-square">
                             <FaSearch className='text-slate-600'></FaSearch>
                         </button>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5 max-w-[1300px]  mx-auto">
                 {
-                    allBlogs.map(allBlog => <AllBlogcCart key={allBlog.id} allBlog={allBlog}></AllBlogcCart>)
+                    allBlogs  .filter((item) => {
+                        return searchTerm.toLowerCase() === ''
+                          ? item
+                          : item.title.toLowerCase().includes(searchTerm);
+                      }) 
+                    .map(allBlog => <AllBlogcCart key={allBlog.id} allBlog={allBlog}></AllBlogcCart>)
                 }
             </div>
 
