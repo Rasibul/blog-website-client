7afb4324699291f0a970ebe264177b23a1fcce7d
Rasibul <rasibul179@gmail.com>
2023-11-08 00:28:49 +0600

    added framer motion package and use home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,23 +3,25 @@ import Banner from "./Banner";
 import BlogCart from "../../Components/BlogCart/BlogCart";
 import Newletter from "../../Components/Newletter";
 import Testimonial from "../../Components/Testimonial/Testimonial";
+import { motion } from 'framer-motion';
 
 const Home = () => {
   const blogs = useLoaderData()
   return (
     <>
-      <Banner></Banner>
-      <div>
-      <h2 className="text-center text-base sm:text-3xl font-bold py-5">Recent Blogs</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5 max-w-[1300px]  mx-auto">
-                {
-                  blogs.map(blog => <BlogCart key={blog.id} blog={blog}></BlogCart>)
-                }
-            </div>
-
-      </div>
-      <Newletter></Newletter>
-      <Testimonial></Testimonial>
+      <motion.div initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 10 }} >
+        <Banner></Banner>
+        <h2 className="text-center text-base sm:text-3xl font-bold py-5">Recent Blogs</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-5 max-w-[1300px]  mx-auto">
+          {
+            blogs.map(blog => <BlogCart key={blog.id} blog={blog}></BlogCart>)
+          }
+        </div>
+        <Newletter></Newletter>
+        <Testimonial></Testimonial>
+      </motion.div>
     </>
   );
 };
